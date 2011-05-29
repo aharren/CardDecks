1f6079c3b7095e51c413f12a56ba802588db0843
Arne Harren <ah@0xc0.de>
2011-05-29 21:32:15 +0200

    ignore some unused device orientations

--- Classes/CDXAppWindowManager.m
+++ Classes/CDXAppWindowManager.m
@@ -42,6 +42,7 @@ synthesize_singleton(sharedAppWindowManager, CDXAppWindowManager);
     if ((self = [super init])) {
         ivar_assign(navigationController, [[UINavigationController alloc] init]);
         navigationController.toolbarHidden = NO;
+        deviceOrientation = UIDeviceOrientationPortrait;
         
         [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(deviceOrientationDidChange:) name:UIDeviceOrientationDidChangeNotification object:nil];
@@ -165,6 +166,15 @@ synthesize_singleton(sharedAppWindowManager, CDXAppWindowManager);
     UIDeviceOrientation newDeviceOrientation = [[UIDevice currentDevice] orientation];
     if (newDeviceOrientation == deviceOrientation) {
         return;
+    }
+    switch (newDeviceOrientation) {
+        case UIDeviceOrientationPortrait:
+        case UIDeviceOrientationLandscapeLeft:
+        case UIDeviceOrientationPortraitUpsideDown:
+        case UIDeviceOrientationLandscapeRight:
+            break;
+        default:
+            return;
     }
     qltrace();
     
