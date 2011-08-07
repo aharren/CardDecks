f9974de6ce53bc7e5f358568abf173ee06f0b69f
Arne Harren <ah@0xc0.de>
2011-08-07 12:14:10 +0200

    update LibComponentLogging-qlog to 1.0.3

--- Logging/qlog.h
+++ Logging/qlog.h
@@ -1,9 +1,9 @@
 //
 //
-// qlog.h -- 1.0.1
+// qlog.h -- 1.0.3
 //
 //
-// Copyright (c) 2009-2010 Arne Harren <ah@0xc0.de>
+// Copyright (c) 2009-2011 Arne Harren <ah@0xc0.de>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -32,25 +32,57 @@
 // messages and qltrace() for trace messages. Additionally, all logging macros
 // take the current log component from the ql_component preprocessor define
 // which can be (re)defined in your application at a file-level, section-based,
-// or global scope.
+// or global scope. If you want to include the log component in your logging
+// statements instead of using the ql_component define, you can use the _c
+// variants of the logging macros which take the log component as the first
+// argument, e.g. qlerror_c(lcl_cMain), qltrace_c(lcl_cMain, @"message").
+//
+
+
+//
+// qlog macros which use the currently active ql_component
 //
 
 
 #define qlcritical(...)                                                        \
-    lcl_log(ql_component, lcl_vCritical, @"" __VA_ARGS__);
+    lcl_log(ql_component, lcl_vCritical, @"" __VA_ARGS__)
 
 #define qlerror(...)                                                           \
-    lcl_log(ql_component, lcl_vError, @"" __VA_ARGS__);
+    lcl_log(ql_component, lcl_vError, @"" __VA_ARGS__)
 
 #define qlwarning(...)                                                         \
-    lcl_log(ql_component, lcl_vWarning, @"" __VA_ARGS__);
+    lcl_log(ql_component, lcl_vWarning, @"" __VA_ARGS__)
 
 #define qlinfo(...)                                                            \
-    lcl_log(ql_component, lcl_vInfo, @"" __VA_ARGS__);
+    lcl_log(ql_component, lcl_vInfo, @"" __VA_ARGS__)
 
 #define qldebug(...)                                                           \
-    lcl_log(ql_component, lcl_vDebug, @"" __VA_ARGS__);
+    lcl_log(ql_component, lcl_vDebug, @"" __VA_ARGS__)
 
 #define qltrace(...)                                                           \
-    lcl_log(ql_component, lcl_vTrace, @"" __VA_ARGS__);
+    lcl_log(ql_component, lcl_vTrace, @"" __VA_ARGS__)
+
+
+//
+// qlog-_c macros which take the log component as first argument
+//
+
+
+#define qlcritical_c(log_component, ...)                                       \
+    lcl_log(log_component, lcl_vCritical, @"" __VA_ARGS__)
+
+#define qlerror_c(log_component, ...)                                          \
+    lcl_log(log_component, lcl_vError, @"" __VA_ARGS__)
+
+#define qlwarning_c(log_component, ...)                                        \
+    lcl_log(log_component, lcl_vWarning, @"" __VA_ARGS__)
+
+#define qlinfo_c(log_component, ...)                                           \
+    lcl_log(log_component, lcl_vInfo, @"" __VA_ARGS__)
+
+#define qldebug_c(log_component, ...)                                          \
+    lcl_log(log_component, lcl_vDebug, @"" __VA_ARGS__)
+
+#define qltrace_c(log_component, ...)                                          \
+    lcl_log(log_component, lcl_vTrace, @"" __VA_ARGS__)
 
