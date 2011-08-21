fe4ff721a8db6dbb6722db34e0b88e58cf97acf4
Arne Harren <ah@0xc0.de>
2011-08-21 09:39:34 +0200

    don't show notice views with the table view as parent

--- Classes/Controllers/CDXCardDeckListViewController.m
+++ Classes/Controllers/CDXCardDeckListViewController.m
@@ -83,7 +83,7 @@
     [super viewDidAppear:animated];
     if ([cardDeck isShuffled]) {
         if (!viewWasAlreadyVisible) {
-            [[CDXAppWindowManager sharedAppWindowManager] showNoticeWithImageNamed:@"Notice-Shuffle.png" text:@"shuffle" timeInterval:0.4 orientation:UIDeviceOrientationFaceUp view:viewTableView];
+            [[CDXAppWindowManager sharedAppWindowManager] showNoticeWithImageNamed:@"Notice-Shuffle.png" text:@"shuffle" timeInterval:0.4 orientation:UIDeviceOrientationFaceUp view:self.view];
         }
     }
     viewWasAlreadyVisible = YES;
@@ -304,10 +304,10 @@
 - (IBAction)shuffleButtonPressed {
     if ([cardDeck isShuffled]) {
         [cardDeck sort];
-        [[CDXAppWindowManager sharedAppWindowManager] showNoticeWithImageNamed:@"Notice-Sort.png" text:@"sort" timeInterval:0.4 orientation:UIDeviceOrientationFaceUp view:viewTableView];
+        [[CDXAppWindowManager sharedAppWindowManager] showNoticeWithImageNamed:@"Notice-Sort.png" text:@"sort" timeInterval:0.4 orientation:UIDeviceOrientationFaceUp view:self.view];
     } else {
         [cardDeck shuffle];
-        [[CDXAppWindowManager sharedAppWindowManager] showNoticeWithImageNamed:@"Notice-Shuffle.png" text:@"shuffle" timeInterval:0.4 orientation:UIDeviceOrientationFaceUp view:viewTableView];
+        [[CDXAppWindowManager sharedAppWindowManager] showNoticeWithImageNamed:@"Notice-Shuffle.png" text:@"shuffle" timeInterval:0.4 orientation:UIDeviceOrientationFaceUp view:self.view];
     }
     [cardDeck updateStorageObjectDeferred:YES];
     
