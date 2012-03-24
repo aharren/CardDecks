27ecfcea206c1fefa83f8c687ba5a532856f542b
Arne Harren <ah@0xc0.de>
2012-03-24 18:51:54 +0100

    fix cell backgrounds for grouping

--- Classes/Controllers/CDXListViewControllerBase.m
+++ Classes/Controllers/CDXListViewControllerBase.m
@@ -95,13 +95,15 @@
     ivar_assign_and_retain(tableCellTextTextColorActionInactive, [UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1.0]);
     ivar_assign_and_retain(tableCellDetailTextFont, [UIFont systemFontOfSize:10]);
     ivar_assign_and_retain(tableCellDetailTextTextColor, [UIColor grayColor]);
-    if (![[CDXDevice sharedDevice] useReducedGraphicsEffects]) {
-        CGFloat rowHeight = viewTableView.rowHeight;
-        ivar_assign_and_retain(tableCellBackgroundImage, [[CDXImageFactory sharedImageFactory] imageForLinearGradientWithTopColor:[CDXColor colorWhite] bottomColor:[CDXColor colorWithRed:0xf8 green:0xf8 blue:0xf8 alpha:0xff] height:rowHeight base:0.75]);
-        ivar_assign_and_retain(tableCellBackgroundImageAlt, [[CDXImageFactory sharedImageFactory] imageForLinearGradientWithTopColor:[CDXColor colorWithRed:0xf0 green:0xf0 blue:0xf0 alpha:0xff] bottomColor:[CDXColor colorWithRed:0xe8 green:0xe8 blue:0xe8 alpha:0xff] height:rowHeight base:0.75]);
+    CGFloat rowHeight = viewTableView.rowHeight;
+    BOOL useReducedGraphicsEffects = [[CDXDevice sharedDevice] useReducedGraphicsEffects];
+    CGFloat base = useReducedGraphicsEffects ? 1.0 : 0.75;
+    if (!useReducedGraphicsEffects) {
+        ivar_assign_and_retain(tableCellBackgroundImage, [[CDXImageFactory sharedImageFactory] imageForLinearGradientWithTopColor:[CDXColor colorWhite] bottomColor:[CDXColor colorWithRed:0xf8 green:0xf8 blue:0xf8 alpha:0xff] height:rowHeight base:base]);
     }
+    ivar_assign_and_retain(tableCellBackgroundImageAlt, [[CDXImageFactory sharedImageFactory] imageForLinearGradientWithTopColor:[CDXColor colorWithRed:0xf0 green:0xf0 blue:0xf0 alpha:0xff] bottomColor:[CDXColor colorWithRed:0xe8 green:0xe8 blue:0xe8 alpha:0xff] height:rowHeight base:base]);
     tableCellImageSize = CGSizeMake(10, 10);
-
+    
     CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
     viewTableView.backgroundView = [[[UIImageView alloc] initWithImage:[[CDXImageFactory sharedImageFactory] imageForLinearGradientWithTopColor:[CDXColor colorWhite] bottomColor:[CDXColor colorWithRed:0xf8 green:0xf8 blue:0xf8 alpha:0xff] height:screenHeight base:0.0]] autorelease];
 }
