4a0f92ab41544e04747c1a2a5ddaf73527350d46
Arne Harren <ah@0xc0.de>
2014-01-01 21:41:29 +0100

    update copyright year

--- Classes/Keyboards/CDXKeyboardExtensions.m
+++ Classes/Keyboards/CDXKeyboardExtensions.m
@@ -3,7 +3,7 @@
 // CDXKeyboardExtensions.m
 //
 //
-// Copyright (c) 2009-2012 Arne Harren <ah@0xc0.de>
+// Copyright (c) 2009-2014 Arne Harren <ah@0xc0.de>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
