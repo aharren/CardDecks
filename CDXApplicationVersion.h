60e139708364e5bca2cb205d5aa00a11b66370f8
Arne Harren <ah@0xc0.de>
2020-05-21 10:07:48 +0200

    3.4.0-dev

--- CDXApplicationVersion.h
+++ CDXApplicationVersion.h
@@ -26,9 +26,9 @@
 
 // Definition of version information (major, minor, etc.).
 #define CDXApplicationVersionMajor  3
-#define CDXApplicationVersionMinor  3
-#define CDXApplicationVersionBuild  1
-#define CDXApplicationVersionSuffix ""
+#define CDXApplicationVersionMinor  4
+#define CDXApplicationVersionBuild  0
+#define CDXApplicationVersionSuffix "-dev"
 
 // NSString with complete version information (<major>.<minor>.<build><suffix>).
 #define CDXApplicationVersion                                                  \
