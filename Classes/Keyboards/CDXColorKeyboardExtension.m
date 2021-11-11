568300a6bc51008c508a8745855a443600465037
Arne Harren <ah@0xc0.de>
2021-11-11 19:42:34 +0100

    use system colors in the colors keyboard extension

--- Classes/Keyboards/CDXColorKeyboardExtension.m
+++ Classes/Keyboards/CDXColorKeyboardExtension.m
@@ -123,17 +123,18 @@ static CDXColorRGB colorChooserSimpleColors[] = {
     ivar_assign(toolbarActiveButtonMarker, [[CDXKeyboardExtensionMarker alloc] init]);
     [toolbar addSubview:toolbarActiveButtonMarker.view];
     self.view.backgroundColor = [[CDXKeyboardExtensions sharedKeyboardExtensions] backgroundColor];
-    NSDictionary *textAttributes = @{ NSFontAttributeName: [UIFont systemFontOfSize:15] };
+    NSDictionary *textAttributes = @{ NSForegroundColorAttributeName:[UIColor systemBlueColor], NSFontAttributeName: [UIFont systemFontOfSize:15] };
+    NSDictionary *textAttributesDisabled = @{ NSForegroundColorAttributeName:[UIColor labelColor], NSFontAttributeName: [UIFont systemFontOfSize:15] };
     [simpleButton setTitleTextAttributes:textAttributes forState:UIControlStateNormal];
-    [simpleButton setTitleTextAttributes:textAttributes forState:UIControlStateDisabled];
+    [simpleButton setTitleTextAttributes:textAttributesDisabled forState:UIControlStateDisabled];
     [simpleButton setTitleTextAttributes:textAttributes forState:UIControlStateHighlighted];
     [simpleButton setTitleTextAttributes:textAttributes forState:UIControlStateFocused];
     [textButton setTitleTextAttributes:textAttributes forState:UIControlStateNormal];
-    [textButton setTitleTextAttributes:textAttributes forState:UIControlStateDisabled];
+    [textButton setTitleTextAttributes:textAttributesDisabled forState:UIControlStateDisabled];
     [textButton setTitleTextAttributes:textAttributes forState:UIControlStateHighlighted];
     [textButton setTitleTextAttributes:textAttributes forState:UIControlStateFocused];
     [backgroundButton setTitleTextAttributes:textAttributes forState:UIControlStateNormal];
-    [backgroundButton setTitleTextAttributes:textAttributes forState:UIControlStateDisabled];
+    [backgroundButton setTitleTextAttributes:textAttributesDisabled forState:UIControlStateDisabled];
     [backgroundButton setTitleTextAttributes:textAttributes forState:UIControlStateHighlighted];
     [backgroundButton setTitleTextAttributes:textAttributes forState:UIControlStateFocused];
     [colorChooserRGBSliderRed setThumbImage:[UIImage imageNamed:@"Circle"] forState:UIControlStateNormal];
@@ -170,7 +171,7 @@ static CDXColorRGB colorChooserSimpleColors[] = {
     
     button.enabled = NO;
     [self.view addSubview:view];
-    view.frame = CGRectMake(0, CGRectGetHeight(toolbar.bounds) + 2, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds) - CGRectGetHeight(toolbar.bounds) - 2);
+    view.frame = CGRectMake(0, CGRectGetHeight(toolbar.bounds) + 1, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds) - CGRectGetHeight(toolbar.bounds) - 1);
     
     [toolbarActiveButtonMarker positionAtBarButtonItem:button animated:animated];
 }
